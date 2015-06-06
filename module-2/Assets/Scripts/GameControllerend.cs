daaae49a997ee633e7c461a67f6706626c8dc810
divya2661 <divn2661@gmail.com>
2015-06-06 12:35:37 +0530

    module-2 Revised

---
+++ module-2/Assets/Scripts/GameControllerend.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerend : MonoBehaviour {
+	
+	public Camera cam;
+	
+	// Use this for initialization
+	void Start(){
+		if (cam == null) {
+			cam = Camera.main;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.F1)) 
+		{	
+			
+			Application.LoadLevel("level-1");
+		}
+		
+	}
+}
